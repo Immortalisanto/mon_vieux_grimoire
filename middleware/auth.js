50d762c0e4d7ca5c83d2928d0ed348d39faa6504
Immortalisanto <tur.anthony91@gmail.com>
2023-05-30 14:50:51 +0200

    Ajout de variables d'environnement

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./.env.local" });
 
 module.exports = (req, res, next) => {
     try {
         console.log("Début de l'auth");
 
         const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId,
