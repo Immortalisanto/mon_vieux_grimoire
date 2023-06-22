dd0e809f0454921baa11e1807bab5fa01ccaa15d
Immortalisanto <tur.anthony91@gmail.com>
2023-06-22 12:23:27 +0200

    Suppression des reqexp responsablent des erreurs

--- models/Book.js
+++ models/Book.js
@@ -6,27 +6,15 @@ const bookSchema = mongoose.Schema({
     title: {
         type: String,
         required: true,
-        match: [
-            /^[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\s']{,50}$/,
-            "Le format du titre n'est pas valide",
-        ],
     },
     author: {
         type: String,
         required: true,
-        match: [
-            /^[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\s.-]{,40}$/,
-            "Le format de l'auteur n'est pas valide",
-        ],
     },
     year: { type: Number, required: true, match: [/^[0-9]{1,4}$/, "Le format de l'année n'est pas valide"] },
     genre: {
         type: String,
         required: true,
-        match: [
-            /^[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\s-]{,30}$/,
-            "Le format du genre n'est pas valide",
-        ],
     },
     ratings: [
         {
